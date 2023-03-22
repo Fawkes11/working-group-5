2f959dd7ab5e3cbecb2c32f6dd1aa1e300d4f53a
Manuel Pino <69331127+Manuelpino1914@users.noreply.github.com>
2023-03-22 17:45:07 -0500

    Carousel home
    Se intenta crear el carousel

--- index.js
+++ index.js
@@ -198,3 +198,33 @@ const fechaActual = data.fechaActual;
     `
     return div
   }
+
+ // Carousel Home
+  var swiper = new swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    slidesPerView: 1,
+    spaceBetween: 10,
+  
+    
+    breakpoints: {
+      620: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+      },
+      680: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+      },
+      920: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+      },
+      1240: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+      },
+    } 
+      });
