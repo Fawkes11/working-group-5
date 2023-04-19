0a1df2063fc39434f235a3a095c7f5f6c0260f5a
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2023-04-19 18:23:48 -0500

    Agregando comentarios para lectura fácil del código

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,46 +1,52 @@
 import { data } from './data.js'
 
-let arrayHome = [];
+/* ---------- Array principal ---------- */
+let arrayEventos = [];
 
 for (let i = 0; i < data.eventos.length; i++) {
 
-  arrayHome.push(data.eventos[i]);
+  arrayEventos.push(data.eventos[i]);
 
 }
-console.log(arrayHome);
+console.log(arrayEventos);
 
-for (let i = 0; i < arrayHome.length; i++) {
+/* ---------- Creación de CARDS ---------- */
+for (let i = 0; i < arrayEventos.length; i++) {
 
   let id = `card${i+1}`
-  let div = createLetters(id, arrayHome[i]);
+  let div = createEventCard(id, arrayEventos[i]);
   document.getElementById('section').appendChild(div)
 
 }
 
-function createLetters(id, objetoData) {
+/* FUNCTION: Retorna UN div para cada evento
+ * Recibe un id que será el identificador del evento
+ * y un objeto que corresponde al evento */
+function createEventCard(id, objetoEvento) {
 
   let div = document.createElement('div');
   div.id = id;
-  div.className = 'swiper-slide'  
+  div.className = 'swiper-slide'
 
-  div.innerHTML = ` 
-      <img src="${objetoData.image}" alt="...">
+  div.innerHTML = `
+      <img src="${objetoEvento.image}" alt="...">
       <div class="card-description">
           <div class="card-title">
-              <h4>${objetoData.name}</h4>
+              <h4>${objetoEvento.name}</h4>
           </div>
           <div class="card-text">
-              <p>${objetoData.description}</p>
+              <p>${objetoEvento.description}</p>
           </div>
           <div class="card-link d-flex justify-content-evenly align-items-center">
-          <h2>${objetoData.price}</h2>
-              <a href="./pages/details.html?id=${objetoData.id}">Ver más</a>
+          <h2>${objetoEvento.price}</h2>
+              <a href="./pages/details.html?id=${objetoEvento.id}">Ver más</a>
           </div>
-      </div>  
+      </div>
 `
   return div
 }
 
+/* ---------- Gestión de complemento para carousel ---------- */
 let swiper = new Swiper('.swiper-container', {
   navigation: {
     nextEl: '.swiper-button-next',
