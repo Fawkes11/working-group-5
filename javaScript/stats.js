053ea3ffbd6f15362272feda58e89c54be08f840
harold alzate <haroldalzate99@gmail.com>
2023-04-25 14:09:48 -0500

    se incorporan funciones en el stast.js se realizan funciones para enviar la informacion requeridad desde la api a la tabla adicial en el stats.html se incorporan id para hacer los ajustes dinamicos

--- javaScript/stats.js
+++ javaScript/stats.js
@@ -1,32 +1,161 @@
-// const id = '6351b0a1b82050da15b3a88a'
-// const url = 'https://pro-talento.up.railway.app/api/amazing/' + id;
+let url = `https://pro-talento.up.railway.app/api/amazing/`;
+let date = '';
+let eventsPast = [];
+let eventsComing = [];
 
-// const answerHiggest = document.getElementById('answerHiggest');
+async function fecthApi() {
+    try {
+        let response = await fetch(url);
+        response = await response.json();
+        console.log(response);
+        date = response.date
+        console.log(date);
+        eventsPast = response.response.filter((item) => item.date <= date)
+        eventsComing = response.response.filter((item) => item.date >= date)
+        console.log(eventsComing);
+        pastName(eventsPast)
+        comingName(eventsComing)
+        higgestAttendance(eventsPast)
 
-// document.addEventListener('DOMContentLoaded', higgestPercentage)
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+fecthApi()
 
 
-// async function higgestPercentage(){
+/*--------------- process past  view the information --------------- */
+function pastName() {
+    const categoryPast = document.getElementById('categoryPast');
+    const percentage = percentagePast();
+    const revenuesDataPast = revenuesPast();
 
-//     try {
-//         let response = await fetch(url); 
-//          response = await response.json() 
-//         let { image, description, date, assistance, capacity, place } = response.response;
-//         // mostrarHTML(response);
-        
-//         const row = document.createElement('tr');
-//         row.innerHTML = `
-//         <td>${assistance}</td>
-//         <td>${assistance}</td>
-//         <td>${assistance}</td>
-//         `
-//         answerHiggest.appendChild(row)
-//     } catch (error) {
-//         console.error(error);
+    for (let i = 0; i < eventsPast.length; i++) {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td style="text-align: center;" >${eventsPast[i].name}</td>
+            <td style="text-align: center;" >${revenuesDataPast[i]}</td>
+            <td style="text-align: center;" >${percentage[i]} %</td>   
+            `
+        categoryPast.appendChild(row)
+    }
+}
+/*-------- percentage past ----------*/
+function percentagePast() {
+    const percentages = [];
+    for (let i = 0; i < eventsPast.length; i++) {
+        const capacity = eventsPast[i].capacity;
+        const assistance = eventsPast[i].assistance
+        // console.log (` esttimate : ${estimate},  capacity: ${capacity}`);
+
+        // Valida que assistance y capacity sean números antes de calcular el porcentaje
+        if (typeof capacity == 'number') {
+            const percentageAtte = ((assistance / capacity) * 100).toFixed(2);
+            percentages.push(percentageAtte);
+        } else {
+            percentages.push('-');
+        }
+
+    }
+    return percentages;
+}
+
+function revenuesPast() {
+    const revenues = [];
+    for (let i = 0; i < eventsPast.length; i++) {
+        const assistance = eventsPast[i].assistance;
+        if (typeof assistance == 'number') {
+            const totalAssitence = assistance;
+            revenues.push(totalAssitence)
+        } else {
+            revenues.push('-');
+        }
+
+    }
+    return revenues;
+}
+
+/*--------------- process upComing  view the information --------------- */
+function comingName() {
+
+    const categoryComing = document.getElementById('categoryComing');
+    const percentages = percentageAttendance();
+    const revenuesData = revenues();
+
+    for (let i = 0; i < eventsComing.length; i++) {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+                <td style="text-align: center;" >${eventsComing[i].name}</td>
+                <td style="text-align: center;" >${revenuesData[i]}</td>
+                <td style="text-align: center;" >${percentages[i]} %</td>   
+                `
+        categoryComing.appendChild(row)
+    }
+
+}
+/*---------- estimate --------------*/
+function revenues() {
+    const revenues = [];
+    for (let i = 0; i < eventsComing.length; i++) {
+        const estimate = eventsComing[i].estimate;
+        if (typeof estimate == 'number') {
+            const totalEstimate = estimate;
+            revenues.push(totalEstimate)
+        } else {
+            revenues.push('-');
+        }
+
+    }
+    return revenues;
+}
+/*---------- percentage attendance --------------*/
+function percentageAttendance() {
+    const percentages = [];
+    for (let i = 0; i < eventsComing.length; i++) {
+        const capacity = eventsComing[i].capacity;
+        const estimate = eventsComing[i].estimate
+        // console.log (` esttimate : ${estimate},  capacity: ${capacity}`);
+
+        // Valida que assistance y capacity sean números antes de calcular el porcentaje
+        if (typeof capacity == 'number') {
+            const percentageAtte = ((estimate / capacity) * 100).toFixed(2);
+            percentages.push(percentageAtte);
+        } else {
+            percentages.push('-');
+        }
+
+    }
+    return percentages;
+}
+
+/*---- -------*/
+function higgestAttendance() {
+    const Higgest = document.getElementById('Higgest');
+    
+
+    for (let i = 0; i < eventsComing.length; i++) {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+                <td style="text-align: center;" ></td>
+                <td style="text-align: center;" ></td>
+                <td style="text-align: center;" ></td>   
+                `
+        Higgest.appendChild(row)
+    }
+}
+
+// function Higgest (){
+//     const Highest = 0;
+
+//     for (let i = 0; i < eventsPast.length; i++) {
+//      let attendancePercentage = (eventsPast[i].assistance / eventsPast[i].capacity)*100;
+     
+//      if (attendancePercentage > Highest) {
+//         Highest = attendancePercentage;
+//      }
+
 //     }
+//     return  ;
 
 // }
-
-
-
-
