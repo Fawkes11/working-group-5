2b22361c5d96e0a9c54816443ee7c6a90c0a5736
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2023-04-26 12:48:10 -0500

    reorganizando funciones para mantenimiento facil del código

--- javaScript/stats.js
+++ javaScript/stats.js
@@ -3,22 +3,32 @@ let date = '';
 let eventsPast = [];
 let eventsComing = [];
 
+/* FUNCTION: fetch API */
 async function fecthApi() {
     try {
         let response = await fetch(url);
         response = await response.json();
         console.log(response);
+
         date = response.date
         console.log(date);
+
         eventsPast = response.response.filter((item) => item.date <= date)
         eventsComing = response.response.filter((item) => item.date >= date)
         console.log(eventsComing);
-        pastName(eventsPast)
-        comingName(eventsComing)
+
+        // section 1
         higgestAttendance(eventsPast)
         let arrayPercentage = namePercentagePast(eventsPast);
         let min = arrayPercentage[arrayPercentage.length-1]
         let max = arrayPercentage[0]
+
+        // section 2
+        comingName(eventsComing)
+
+        // section 3
+        pastName(eventsPast)
+
     } catch (error) {
         console.error(error);
     }
@@ -26,95 +36,43 @@ async function fecthApi() {
 
 fecthApi()
 
-function pastName() {
+/* SECTION 1: Events Statictic */
 
-    const categoryPast = document.getElementById('categoryPast');
-    const percentage = percentagePast();
-    const revenuesDataPast = revenuesPast();
+/* function aux math calcs */
+function namePercentagePast (eventsPast){
 
-    for (let i = 0; i < eventsPast.length; i++) {
-        const row = document.createElement('tr');
-        row.innerHTML = `
-            <td style="text-align: center;" >${eventsPast[i].name}</td>
-            <td style="text-align: center;" >${revenuesDataPast[i]}</td>
-            <td style="text-align: center;" >${percentage[i]} %</td>
-            `
-        categoryPast.appendChild(row)
-    }
+    let epPercetange = eventsPast.map((each)=> {
+
+        const capacity = each.capacity
+        const assistance = each.assistance
+        const percentageAtte = ((assistance / capacity) * 100).toFixed(2);
+        return {name:each.name, porcentage:percentageAtte}
+
+    })
+    epPercetange.sort((a, b) => b.porcentage - a.porcentage)
+
+    return epPercetange;
+
 }
 
-function percentagePast() {
-
-    const percentages = [];
-
-    for (let i = 0; i < eventsPast.length; i++) {
-        const capacity = eventsPast[i].capacity;
-        const assistance = eventsPast[i].assistance
-        // Valida que assistance y capacity sean números antes de calcular el porcentaje
-        if (typeof capacity == 'number') {
-            const percentageAtte = ((assistance / capacity) * 100).toFixed(2);
-            percentages.push(percentageAtte);
-        } else {
-            percentages.push('-');
-        }
-
-    }
-    return percentages;
-}
-
-function revenuesPast() {
-
-    const revenues = [];
-
-    for (let i = 0; i < eventsPast.length; i++) {
-        const assistance = eventsPast[i].assistance;
-        if (typeof assistance == 'number') {
-            const totalAssitence = assistance;
-            revenues.push(totalAssitence)
-        } else {
-            revenues.push('-');
-        }
-
-    }
-    return revenues;
-}
-
-function comingName() {
-
-    const categoryComing = document.getElementById('categoryComing');
-    const percentages = percentageAttendance();
-    const revenuesData = revenues();
+/* function aux for show data */
+function higgestAttendance() {
+    const Higgest = document.getElementById('Higgest');
 
     for (let i = 0; i < eventsComing.length; i++) {
         const row = document.createElement('tr');
         row.innerHTML = `
-                <td style="text-align: center;" >${eventsComing[i].name}</td>
-                <td style="text-align: center;" >${revenuesData[i]}</td>
-                <td style="text-align: center;" >${percentages[i]} %</td>
+                <td style="text-align: center;" ></td>
+                <td style="text-align: center;" ></td>
+                <td style="text-align: center;" ></td>
                 `
-        categoryComing.appendChild(row)
+        Higgest.appendChild(row)
     }
-
 }
 
-function revenues() {
+/* SECTION 2: Upcoming Events statictic by name (TO-DO: category) */
 
-    const revenues = [];
-
-    for (let i = 0; i < eventsComing.length; i++) {
-        const estimate = eventsComing[i].estimate;
-        if (typeof estimate == 'number') {
-            const totalEstimate = estimate;
-            revenues.push(totalEstimate)
-        } else {
-            revenues.push('-');
-        }
-
-    }
-    return revenues;
-}
-
-
+/* function aux 1 */
 function percentageAttendance() {
 
     const percentages = [];
@@ -134,33 +92,97 @@ function percentageAttendance() {
     return percentages;
 }
 
+/* function aux 2 */
+function revenues() {
 
-function higgestAttendance() {
-    const Higgest = document.getElementById('Higgest');
+    const revenues = [];
+
+    for (let i = 0; i < eventsComing.length; i++) {
+        const estimate = eventsComing[i].estimate;
+        if (typeof estimate == 'number') {
+            const totalEstimate = estimate;
+            revenues.push(totalEstimate)
+        } else {
+            revenues.push('-');
+        }
+
+    }
+    return revenues;
+}
+
+/* main function section 2 */
+function comingName() {
+
+    const categoryComing = document.getElementById('categoryComing');
+    const percentages = percentageAttendance();
+    const revenuesData = revenues();
 
     for (let i = 0; i < eventsComing.length; i++) {
         const row = document.createElement('tr');
         row.innerHTML = `
-                <td style="text-align: center;" ></td>
-                <td style="text-align: center;" ></td>
-                <td style="text-align: center;" ></td>
+                <td style="text-align: center;" >${eventsComing[i].name}</td>
+                <td style="text-align: center;" >${revenuesData[i]}</td>
+                <td style="text-align: center;" >${percentages[i]} %</td>
                 `
-        Higgest.appendChild(row)
+        categoryComing.appendChild(row)
     }
+
 }
 
-function namePercentagePast (eventsPast){
+/* SECTION 3: Past Events statictic by name (TO-DO: category) */
 
-    let epPercetange = eventsPast.map((each)=> {
+/* function aux 1 */
+function percentagePast() {
 
-        const capacity = each.capacity
-        const assistance = each.assistance
-        const percentageAtte = ((assistance / capacity) * 100).toFixed(2);
-        return {name:each.name, porcentage:percentageAtte}
+    const percentages = [];
 
-    })
-    epPercetange.sort((a, b) => b.porcentage - a.porcentage)
+    for (let i = 0; i < eventsPast.length; i++) {
+        const capacity = eventsPast[i].capacity;
+        const assistance = eventsPast[i].assistance
+        // Valida que assistance y capacity sean números antes de calcular el porcentaje
+        if (typeof capacity == 'number') {
+            const percentageAtte = ((assistance / capacity) * 100).toFixed(2);
+            percentages.push(percentageAtte);
+        } else {
+            percentages.push('-');
+        }
 
-    return epPercetange;
+    }
+    return percentages;
+}
 
+/* function aux 2 */
+function revenuesPast() {
+
+    const revenues = [];
+
+    for (let i = 0; i < eventsPast.length; i++) {
+        const assistance = eventsPast[i].assistance;
+        if (typeof assistance == 'number') {
+            const totalAssitence = assistance;
+            revenues.push(totalAssitence)
+        } else {
+            revenues.push('-');
+        }
+
+    }
+    return revenues;
+}
+
+/* main function 3 */
+function pastName() {
+
+    const categoryPast = document.getElementById('categoryPast');
+    const percentage = percentagePast();
+    const revenuesDataPast = revenuesPast();
+
+    for (let i = 0; i < eventsPast.length; i++) {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td style="text-align: center;" >${eventsPast[i].name}</td>
+            <td style="text-align: center;" >${revenuesDataPast[i]}</td>
+            <td style="text-align: center;" >${percentage[i]} %</td>
+            `
+        categoryPast.appendChild(row)
+    }
 }
